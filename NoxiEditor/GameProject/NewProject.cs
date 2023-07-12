1e85ee2e3e3c387023707205db7b016e68bf160c
Mathias Clari Drenik <66589714+MathiasClari@users.noreply.github.com>
2023-07-13 00:03:09 +0200

    Updated NewProjectView amd added placeholders for textboxes

---
+++ NoxiEditor/GameProject/NewProject.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoxiEditor.GameProject
+{
+    class NewProject : ViewModelBase
+    {
+        private string _name = "New Project";
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+
+        private string _path = "C:\\";
+
+        public string Path
+        {
+            get => _path;
+            set
+            {
+                if (_path != value)
+                {
+                    _path = value;
+
+                    OnPropertyChanged(nameof(Path));
+                }
+            }
+        }
+    }
+}
